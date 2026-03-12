d24d0aa2f0dfe361ed8b8c8d8a30d13bb563e134
Ksenia Durkina <sigismund1old@gmail.com>
2026-03-12 22:24:22 +0300

    feat: add return 'nameUser' in function sayGreeting

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
+
 const sayGreeting = () => {
     console.log("Welcome to the Brain Games!");
     const nameUser = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${nameUser}!`);
+    return nameUser;
 }
 export default sayGreeting;
