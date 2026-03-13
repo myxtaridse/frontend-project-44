d9f6d476f49a919547a5b2222711537522888310
Ksenia Durkina <sigismund1old@gmail.com>
2026-03-13 09:22:12 +0300

    feat: implement common logic in brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -1,31 +1,15 @@
-import readlineSync from 'readline-sync';
+import runGame from '../index.js';
+
+const description = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
 
 const isEven = (num) => num % 2 === 0;
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1));
 
-const sayEven = (userName) => {
-    console.log("Answer \"yes\" if the number is even, otherwise answer \"no\".");
-    
-    const roundsToWin = 3;
-    let round = 0;
-
-    while (round < roundsToWin) {
-        const randomNumber = getRandomNumber(1, 100); 
-        const rightAnswer = isEven(randomNumber) ? "yes" : "no";
-
-        console.log(`Question: ${randomNumber}`);
-        const answer = readlineSync.question('Your answer: ');
-
-        if (answer === rightAnswer) {
-            console.log("Correct!");
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${userName}!`);
-            return;
-        }
-
-        round += 1;
-    }
-    console.log(`Congratulations, ${userName}!`);
+const getRoundData = () => {
+    const randomNumber = getRandomNumber(1, 100);
+    const rightAnswer = isEven(randomNumber) ? "yes" : "no";
+    return [randomNumber, rightAnswer];
 }
-export default sayEven;
+
+export default () => runGame(description, getRoundData);
